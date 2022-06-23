76f7144e0c90895cfb2c66b452df3b8e6d85a08b
lykimphong2001@gmail.com <lykimphong2001@example.com>
2022-06-23 14:27:57 +0700

    commit cart

--- src/components/page-flow1/cart-flow1/cartFlow1.js
+++ src/components/page-flow1/cart-flow1/cartFlow1.js
@@ -6,8 +6,19 @@ class CartFlow1 extends React.Component {
     render() {
         return(
             <div className="cartflow1"> 
+                <div className="textCart"> Giỏ hàng </div>
+                <div className="item-header">
+                <div className="item-header-icon"> # </div>
+                <div className="item-header-text"> Tên hàng </div>
+                <div className="item-header-text2"> Mã hàng </div>
+                <div className="item-header-text3"> Đơn giá áp dụng </div>
+                <div className="item-header-text4"> Số lượng </div>
+                <div className="item-header-text5"> Trạng thái hàng </div>
+                </div>
+                <div className="item">
 
-                <span className="cartflow1-item"> Giỏ hàng </span>
+                </div>
+                
                 
             </div>
         )
