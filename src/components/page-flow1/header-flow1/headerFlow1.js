1fcd59690d19fb16069e040ea4d1517bbbb245f6
Trần Văn Hoàng <93042377+chamuonfa@users.noreply.github.com>
2022-06-26 16:48:56 +0700

    commit headerFlow1

--- src/components/page-flow1/header-flow1/headerFlow1.js
+++ src/components/page-flow1/header-flow1/headerFlow1.js
@@ -39,6 +39,28 @@ class HeaderFlow1 extends React.Component {
                                 <path id="Path_16446" data-name="Path 16446" d="M10.036,16h-4V13.658a6,6,0,0,1-1.9-1.1L2.107,13.733l-2-3.467L2.136,9.1a6.067,6.067,0,0,1,0-2.193L.107,5.733l2-3.467L4.136,3.439a6,6,0,0,1,1.9-1.1V0h4V2.342a6,6,0,0,1,1.9,1.1l2.029-1.173,2,3.467L13.936,6.9a6.067,6.067,0,0,1,0,2.193l2.029,1.17-2,3.467-2.029-1.172a6,6,0,0,1-1.9,1.1ZM7.369,14.667H8.7V12.649l.5-.129a4.656,4.656,0,0,0,2.162-1.253l.362-.369,1.749,1.01.667-1.155L12.4,9.745l.137-.5a4.7,4.7,0,0,0,0-2.5l-.137-.5,1.747-1.009-.667-1.155L11.727,5.1l-.362-.366A4.656,4.656,0,0,0,9.2,3.483l-.5-.132V1.333H7.369V3.351l-.5.129A4.656,4.656,0,0,0,4.706,4.733L4.344,5.1,2.6,4.089,1.929,5.244,3.676,6.253l-.137.5a4.7,4.7,0,0,0,0,2.5l.137.5L1.929,10.756,2.6,11.911,4.344,10.9l.362.369a4.656,4.656,0,0,0,2.162,1.25l.5.129Zm.667-4A2.667,2.667,0,1,1,10.7,8,2.667,2.667,0,0,1,8.036,10.667Zm0-4A1.333,1.333,0,1,0,9.369,8,1.333,1.333,0,0,0,8.036,6.667Z" transform="translate(0)" fill="#717171"/>
                             </g>
                         </svg>
+                        <div className="setting-list">
+                            <div className="setting-list__update">
+                                <h5>Cập nhập trạng thái</h5>
+                            </div>
+                            <div className="setting-list__name">
+                                <h5>Lorem ipsum</h5>
+                            </div>
+                            <div className="setting-list__reward">
+                                <h5>Ghi nhận công nợ</h5>
+                                <h5>Thanh toán</h5>
+                                <h5>Phát tiền thưởng</h5>
+                            </div>
+                            <div className="setting-list__collect">
+                                <h5>Bỏ ghi công nợ</h5>
+                                <h5>Nhập lại kho</h5>
+                                <h5>Bỏ ghi thanh toán</h5>
+                                <h5>Thu hồi tiền thưởng</h5>
+                            </div>
+                            <div className="setting-list__del">
+                                <h5>Huỷ đơn</h5>
+                            </div>
+                        </div>
                     </div>
                     <Link to='/'>
                         <button className="header-flow1__button-done"> Duyệt đơn </button>
