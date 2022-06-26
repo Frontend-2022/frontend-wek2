e4853cc34ee19cfdf4562496f19f2b26fe1a83c6
lykimphong2001@gmail.com <lykimphong2001@example.com>
2022-06-26 13:40:58 +0700

    commit on dropdown list

--- src/components/page-flow1/contents-flow1/contentsFlow1.js
+++ src/components/page-flow1/contents-flow1/contentsFlow1.js
@@ -7,7 +7,6 @@ import { BiWifi0 } from "react-icons/bi";
 // }
 
 class ContentsFlow1 extends React.Component {
-    
     render() {
         return(
             <div className="contentsflow1">
@@ -59,7 +58,38 @@ class ContentsFlow1 extends React.Component {
                                 <div className="text3">Người bán</div>
                                 <div className="imagegrid">
                                 <div className="image"></div>
-                                <div className="text8">Nguyễn Văn Mười Ba</div>
+                                <div className="text8">Nguyễn Văn Mười Ba
+                                <div className="nav_sub2">
+                                <div class="search-container">
+                                    <input type="text" placeholder="Search.." name="search"></input>
+                                    <button><svg xmlns="http://www.w3.org/2000/svg" id="Group_13362" data-name="Group 13362" width="14" height="14" viewBox="0 0 14 14">
+                                            <rect id="Rectangle_5105" data-name="Rectangle 5105" width="14" height="14" fill="none"/>
+                                            <g id="search-interface-symbol" transform="translate(0)">
+                                                <g id="_x34__4_" transform="translate(0)">
+                                                <g id="Group_11619" data-name="Group 11619">
+                                                    <path id="Path_15839" data-name="Path 15839" d="M13.873,13.239,10.259,9.682A5.717,5.717,0,0,0,11.787,5.8,5.844,5.844,0,0,0,5.9,0,5.844,5.844,0,0,0,.006,5.8a5.844,5.844,0,0,0,5.89,5.8A5.924,5.924,0,0,0,9.6,10.3l3.629,3.571a.458.458,0,0,0,.641,0A.441.441,0,0,0,13.873,13.239ZM5.9,10.7A4.945,4.945,0,0,1,.912,5.8,4.945,4.945,0,0,1,5.9.892,4.945,4.945,0,0,1,10.88,5.8,4.945,4.945,0,0,1,5.9,10.7Z" transform="translate(-0.006 0)" fill="#707070"/>
+                                                </g>
+                                                </g>
+                                            </g>
+                                            </svg></button>
+                                </div>
+                                    <div className="nav_sub-item2 ">
+                                   
+                                    <div className="imagegrid">
+                                    <div className="image"></div>
+                                    Nguyễn Văn Mười Ba
+                                    
+                                    </div>
+                                    </div>
+                                    <div className="nav_sub-item2 ">
+                                
+                                    <div className="imagegrid">
+                                    <div className="image"></div>
+                                    Nguyễn Văn Mười Ba
+                                  
+                                    </div>
+                                    </div>
+                                </div></div>
                                 </div>
                                 </div>
                                 <div className=" block2-1-1-1"> 
@@ -76,7 +106,32 @@ class ContentsFlow1 extends React.Component {
                                 </div>
                                 <div className=" block2-1-1-2">
                                 <div className="text3">Kho bán hàng</div> 
-                                <div className="text9">Lorem ipsum dolor sit amet,</div>
+                                <div className="text9">Lorem ipsum dolor sit amet,
+                                <div className="nav_sub">
+                                <div class="search-container">
+                                    <input type="text" placeholder="Search.." name="search"></input>
+                                    <button><svg xmlns="http://www.w3.org/2000/svg" id="Group_13362" data-name="Group 13362" width="14" height="14" viewBox="0 0 14 14">
+                                            <rect id="Rectangle_5105" data-name="Rectangle 5105" width="14" height="14" fill="none"/>
+                                            <g id="search-interface-symbol" transform="translate(0)">
+                                                <g id="_x34__4_" transform="translate(0)">
+                                                <g id="Group_11619" data-name="Group 11619">
+                                                    <path id="Path_15839" data-name="Path 15839" d="M13.873,13.239,10.259,9.682A5.717,5.717,0,0,0,11.787,5.8,5.844,5.844,0,0,0,5.9,0,5.844,5.844,0,0,0,.006,5.8a5.844,5.844,0,0,0,5.89,5.8A5.924,5.924,0,0,0,9.6,10.3l3.629,3.571a.458.458,0,0,0,.641,0A.441.441,0,0,0,13.873,13.239ZM5.9,10.7A4.945,4.945,0,0,1,.912,5.8,4.945,4.945,0,0,1,5.9.892,4.945,4.945,0,0,1,10.88,5.8,4.945,4.945,0,0,1,5.9,10.7Z" transform="translate(-0.006 0)" fill="#707070"/>
+                                                </g>
+                                                </g>
+                                            </g>
+                                            </svg></button>
+                                </div>
+                                    <div className="nav_sub-item ">
+                                    <div className="nav_sub-item-text ">
+                                    Lorem ipsum dolor sit amet,
+                                    </div>
+                                    </div>
+                                    <div className="nav_sub-item ">
+                                    <div className="nav_sub-item-text ">
+                                    Lorem ipsum dolor sit amet,
+                                    </div>
+                                    </div>
+                                </div></div>
                                 </div> 
                             </div>
                         </div>
@@ -86,14 +141,59 @@ class ContentsFlow1 extends React.Component {
                                 <div className="text4">Phương thức vận chuyển</div>
                                 <div className="text9">Xe công ty
                                 <div className="nav_sub">
+                                <div class="search-container">
+                                    <input type="text" placeholder="Search.." name="search"></input>
+                                    <button><svg xmlns="http://www.w3.org/2000/svg" id="Group_13362" data-name="Group 13362" width="14" height="14" viewBox="0 0 14 14">
+                                            <rect id="Rectangle_5105" data-name="Rectangle 5105" width="14" height="14" fill="none"/>
+                                            <g id="search-interface-symbol" transform="translate(0)">
+                                                <g id="_x34__4_" transform="translate(0)">
+                                                <g id="Group_11619" data-name="Group 11619">
+                                                    <path id="Path_15839" data-name="Path 15839" d="M13.873,13.239,10.259,9.682A5.717,5.717,0,0,0,11.787,5.8,5.844,5.844,0,0,0,5.9,0,5.844,5.844,0,0,0,.006,5.8a5.844,5.844,0,0,0,5.89,5.8A5.924,5.924,0,0,0,9.6,10.3l3.629,3.571a.458.458,0,0,0,.641,0A.441.441,0,0,0,13.873,13.239ZM5.9,10.7A4.945,4.945,0,0,1,.912,5.8,4.945,4.945,0,0,1,5.9.892,4.945,4.945,0,0,1,10.88,5.8,4.945,4.945,0,0,1,5.9,10.7Z" transform="translate(-0.006 0)" fill="#707070"/>
+                                                </g>
+                                                </g>
+                                            </g>
+                                            </svg></button>
+                                </div>
                                     <div className="nav_sub-item ">
-                                     Our Company
+                                    <div className="nav_sub-item-text ">
+                                    Xe công ty
+                                    </div>
+                                    </div>
+                                    <div className="nav_sub-item ">
+                                    <div className="nav_sub-item-text ">
+                                    Xe công ty
+                                    </div>
                                     </div>
                                 </div></div>
                                 </div>
                                 <div className=" block2-2-1-1"> 
                                 <div className="text4">Hình thức thanh toán</div>
-                                <div className="text9">Thanh toán khi nhận hàng (COD)</div>
+                                <div className="text9">Thanh toán khi nhận hàng (COD)
+                                <div className="nav_sub">
+                                <div class="search-container">
+                                    <input type="text" placeholder="Search.." name="search"></input>
+                                    <button><svg xmlns="http://www.w3.org/2000/svg" id="Group_13362" data-name="Group 13362" width="14" height="14" viewBox="0 0 14 14">
+                                            <rect id="Rectangle_5105" data-name="Rectangle 5105" width="14" height="14" fill="none"/>
+                                            <g id="search-interface-symbol" transform="translate(0)">
+                                                <g id="_x34__4_" transform="translate(0)">
+                                                <g id="Group_11619" data-name="Group 11619">
+                                                    <path id="Path_15839" data-name="Path 15839" d="M13.873,13.239,10.259,9.682A5.717,5.717,0,0,0,11.787,5.8,5.844,5.844,0,0,0,5.9,0,5.844,5.844,0,0,0,.006,5.8a5.844,5.844,0,0,0,5.89,5.8A5.924,5.924,0,0,0,9.6,10.3l3.629,3.571a.458.458,0,0,0,.641,0A.441.441,0,0,0,13.873,13.239ZM5.9,10.7A4.945,4.945,0,0,1,.912,5.8,4.945,4.945,0,0,1,5.9.892,4.945,4.945,0,0,1,10.88,5.8,4.945,4.945,0,0,1,5.9,10.7Z" transform="translate(-0.006 0)" fill="#707070"/>
+                                                </g>
+                                                </g>
+                                            </g>
+                                            </svg></button>
+                                </div>
+                                    <div className="nav_sub-item ">
+                                    <div className="nav_sub-item-text ">
+                                    Thanh toán khi nhận hàng (COD)
+                                    </div>
+                                    </div>
+                                    <div className="nav_sub-item ">
+                                    <div className="nav_sub-item-text ">
+                                    Thanh toán khi nhận hàng (COD)
+                                    </div>
+                                    </div>
+                                </div></div>
                                 </div>
                             </div>
                             <div className="block2-2-2"> 
