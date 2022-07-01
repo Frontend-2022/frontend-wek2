f5aacb4cf24e56680c8ead59d638496665f21611
Trần Văn Hoàng <93042377+chamuonfa@users.noreply.github.com>
2022-07-01 11:14:52 +0700

    commit filter

--- src/components/Navigation/nav.js
+++ src/components/Navigation/nav.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React,{useState} from "react";
 import './nav.css';
 import { FiChevronLeft } from "react-icons/fi";
 
 function Nav() {
+    const [state,setState] =useState(true);
+    const clickFilter=() =>{
+        setState (!state);
+    }
+
     return(
         <div class="navigation">
             
@@ -19,23 +24,25 @@ function Nav() {
 
             <div class="navigation-boder"></div>
 
-            <div class="navigation__name">
-                <svg className="navigation__name-icon1" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12">
-                    <g id="exclamation_6_" data-name="exclamation (6)" transform="translate(0 0)">
-                        <path id="Path_16703" data-name="Path 16703" d="M6,0a6,6,0,1,0,6,6A6,6,0,0,0,6,0ZM6,11a5,5,0,1,1,5-5A5,5,0,0,1,6,11Z" fill="rgba(0,127,255,0)"/>
-                        <path id="Path_16704" data-name="Path 16704" d="M11.5,5a.5.5,0,0,0-.5.5v4a.5.5,0,0,0,1,0v-4A.5.5,0,0,0,11.5,5Z" transform="translate(-5.5 -2.5)" fill="#fff"/>
-                        <rect id="Rectangle_8071" data-name="Rectangle 8071" width="1" height="1" rx="0.5" transform="translate(5.5 8.5)" fill="#fff"/>
-                    </g>
-                </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14">
-                    <g id="settings-sliders_1_" data-name="settings-sliders (1)" transform="translate(0 0)">
-                        <path id="Path_16699" data-name="Path 16699" d="M.583,2.771h1.6a2.175,2.175,0,0,0,4.2,0h7.04a.583.583,0,0,0,0-1.167H6.376a2.175,2.175,0,0,0-4.2,0H.583a.583.583,0,0,0,0,1.167Zm3.694-1.6A1.021,1.021,0,1,1,3.257,2.187,1.021,1.021,0,0,1,4.277,1.167Z" fill="#141414"/>
-                        <path id="Path_16700" data-name="Path 16700" d="M13.416,9.854h-1.6a2.174,2.174,0,0,0-4.2,0H.583a.583.583,0,1,0,0,1.167H7.624a2.174,2.174,0,0,0,4.2,0h1.6a.583.583,0,1,0,0-1.167Zm-3.694,1.6a1.021,1.021,0,1,1,1.021-1.021,1.021,1.021,0,0,1-1.021,1.021Z" transform="translate(0 -3.437)" fill="#141414"/>
-                        <path id="Path_16701" data-name="Path 16701" d="M13.416,18.1H6.376a2.175,2.175,0,0,0-4.2,0H.583a.583.583,0,1,0,0,1.167h1.6a2.175,2.175,0,0,0,4.2,0h7.04a.583.583,0,1,0,0-1.167Zm-9.139,1.6A1.021,1.021,0,1,1,5.3,18.687,1.021,1.021,0,0,1,4.277,19.708Z" transform="translate(0 -6.875)" fill="#141414"/>
-                    </g>
-                </svg>
-                <span className="navigation__name-filter">Bộ lọc</span>
-            </div>
+            {/* <div class="navigation__name"> */}
+                <button onClick={clickFilter} className= {'navigation__name' + (state ? '': ' navigation__name-hidden navigation__name-icon2')}>
+                    <svg className="navigation__name-icon1 navigation__name-icon2" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12">
+                        <g id="exclamation_6_" data-name="exclamation (6)" transform="translate(0 0)">
+                            <path id="Path_16703" data-name="Path 16703" d="M6,0a6,6,0,1,0,6,6A6,6,0,0,0,6,0ZM6,11a5,5,0,1,1,5-5A5,5,0,0,1,6,11Z" fill="rgba(0,127,255,0)"/>
+                            <path id="Path_16704" data-name="Path 16704" d="M11.5,5a.5.5,0,0,0-.5.5v4a.5.5,0,0,0,1,0v-4A.5.5,0,0,0,11.5,5Z" transform="translate(-5.5 -2.5)" fill="#fff"/>
+                            <rect id="Rectangle_8071" data-name="Rectangle 8071" width="1" height="1" rx="0.5" transform="translate(5.5 8.5)" fill="#fff"/>
+                        </g>
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14">
+                        <g id="settings-sliders_1_" data-name="settings-sliders (1)" transform="translate(0 0)">
+                            <path id="Path_16699" data-name="Path 16699" d="M.583,2.771h1.6a2.175,2.175,0,0,0,4.2,0h7.04a.583.583,0,0,0,0-1.167H6.376a2.175,2.175,0,0,0-4.2,0H.583a.583.583,0,0,0,0,1.167Zm3.694-1.6A1.021,1.021,0,1,1,3.257,2.187,1.021,1.021,0,0,1,4.277,1.167Z" fill="#141414"/>
+                            <path id="Path_16700" data-name="Path 16700" d="M13.416,9.854h-1.6a2.174,2.174,0,0,0-4.2,0H.583a.583.583,0,1,0,0,1.167H7.624a2.174,2.174,0,0,0,4.2,0h1.6a.583.583,0,1,0,0-1.167Zm-3.694,1.6a1.021,1.021,0,1,1,1.021-1.021,1.021,1.021,0,0,1-1.021,1.021Z" transform="translate(0 -3.437)" fill="#141414"/>
+                            <path id="Path_16701" data-name="Path 16701" d="M13.416,18.1H6.376a2.175,2.175,0,0,0-4.2,0H.583a.583.583,0,1,0,0,1.167h1.6a2.175,2.175,0,0,0,4.2,0h7.04a.583.583,0,1,0,0-1.167Zm-9.139,1.6A1.021,1.021,0,1,1,5.3,18.687,1.021,1.021,0,0,1,4.277,19.708Z" transform="translate(0 -6.875)" fill="#141414"/>
+                        </g>
+                    </svg>
+                    <span className="navigation__name-filter">Bộ lọc</span>
+                </button>
+            {/* </div> */}
             
         </div>
     )
