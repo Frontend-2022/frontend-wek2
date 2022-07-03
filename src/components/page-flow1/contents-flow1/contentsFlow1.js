7f5b3a5528758127c0cfeaeb64d179486d1f798d
Trần Văn Hoàng <93042377+chamuonfa@users.noreply.github.com>
2022-07-03 17:11:41 +0700

    commit

--- src/components/page-flow1/contents-flow1/contentsFlow1.js
+++ src/components/page-flow1/contents-flow1/contentsFlow1.js
@@ -32,6 +32,7 @@ function ContentsFlow1() {
         }   
         setAppear(!appear);
     }
+
     const makeappear2 = (id,id1) => {
         if (appear) {
             document.getElementById(id).style.display = 'block';
@@ -91,13 +92,14 @@ function ContentsFlow1() {
                     <div className="block2-1">
                         <div className=" block2-1-1">
                             
+                            {/* <div onClick={() => makeappear3('test-full-screen')} className="full-screen"></div> */}
+
                             <div onClick={() => makeappear1('test-hover1')}  className=" block2-1-1-1 block2-1-1-1-hover">
                                 <div className="text3">Người bán</div>
                                 <div className="imagegrid">
                                     <div className="image"></div>
                                     <div className="text8">Nguyễn Văn Mười Ba</div>
                                 </div>
-
                                 <div id="test-hover1" className="test-hover">
                                     <div class="search-container2">
                                         <div className="input-grid">
